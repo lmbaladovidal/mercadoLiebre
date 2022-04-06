1eef235dfe1f4e44b9550edfebe467e6a661eb1a
Leandro Martín Balado Vidal <lm.baladovidal@gmail.com>
2022-04-05 22:56:08 -0300

    subiendo archivos para deploy

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const publicPath = path.resolve(__dirname,"./public");
 app.use(express.static(publicPath))
-app.listen(3000,()=>console.log("Running at 3000 port"));
+app.listen(process.env.PORT || 3000,()=>console.log("Running at 3000 port"));
 app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,("./views/home.html")))
 })
